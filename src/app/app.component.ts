5cee2c621a245a1fd622fbfa35a23c1f1d0e4714
ejssantos <48992083+ejssantos@users.noreply.github.com>
2020-08-12 16:13:37 -0300

    Projeto Angular 10 com DataTables

---
+++ src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Angular10Datatables';
+
+  public data = [
+    {name: 'Ajay', email: 'therichpost@gmail.com', website: 'therichpost.com'},
+    {name: 'Jas', email: 'therichpost@gmail.com', website: 'therichpost.com'},
+    {name: 'therichpost', email: 'therichpost@gmail.com', website: 'therichpost.com'},
+    {name: 'therichpost', email: 'therichpost@gmail.com', website: 'therichpost.com'},
+    {name: 'Jas', email: 'therichpost@gmail.com', website: 'therichpost.com'},
+    {name: 'therichpost', email: 'therichpost@gmail.com', website: 'therichpost.com'},
+    {name: 'therichpost', email: 'therichpost@gmail.com', website: 'therichpost.com'},
+    {name: 'Jas', email: 'therichpost@gmail.com', website: 'therichpost.com'},
+    {name: 'therichpost', email: 'therichpost@gmail.com', website: 'therichpost.com'},
+    {name: 'therichpost', email: 'therichpost@gmail.com', website: 'therichpost.com'},
+  ];
+
+  dtOptions: DataTables.Settings = {};
+
+  ngOnInit() {
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      pageLength: 5,
+      lengthMenu: [5, 10, 25],
+      processing: true
+    };
+  }
+}
